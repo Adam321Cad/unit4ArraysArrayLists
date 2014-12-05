b3cdb41ae7068ac7e9d90e356e058aef426ad2b0
Adam321Cad <aratoad@gmail.com>
2014-12-05 13:11:32 -0600

    not done
    not done

---
+++ Unit4Programs/Customers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Customers
+{
+    /** description of instance variable x (add comment for each instance variable) */
+    private String name;
+
+    /**
+     * Default constructor for objects of class Customers
+     */
+    public Customers(String name)
+    {
+        // initialise instance variables
+        this.name = name;
+    }
+
+
+}
---
+++ Unit4Programs/Shop.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Shop {
+    public Shop(){
+        Customers c = new Customers();
+        ArrayList<Customers> customers = new ArrayList();
+        
+    }
+}
